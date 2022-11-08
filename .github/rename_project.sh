5ba59e63e6548c2bd441b8758fdeb01d44e0a8c1
awicenec <awicenec@users.noreply.github.com>
2022-11-08 04:08:10 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="ICRAR"
+original_name="daliuge_gen_pal"
+original_urlname="daliuge-gen-pal"
+original_description="Awesome daliuge_gen_pal created by ICRAR"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv daliuge_gen_pal $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
--- daliuge_gen_pal/cli.py
+++ daliuge_gen_pal/cli.py
@@ -1,4 +1,4 @@
-"""CLI interface for project_name project.
+"""CLI interface for daliuge_gen_pal project.
 
 Be creative! do whatever you want!
 
@@ -12,7 +12,7 @@ Be creative! do whatever you want!
 def main():  # pragma: no cover
     """
     The main function executes on commands:
-    `python -m project_name` and `$ project_name `.
+    `python -m daliuge_gen_pal` and `$ daliuge_gen_pal `.
 
     This is your program's entry point.
 
