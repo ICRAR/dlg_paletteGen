668769a917a03b4b28ad3f167bbf2687c322f683
james-strauss-uwa <james.strauss@uwa.edu.au>
2022-12-21 19:08:23 +0800

    Fix for black issue

--- dlg_paletteGen/base.py
+++ dlg_paletteGen/base.py
@@ -954,7 +954,8 @@ class DetailedDescription:
             else:
                 logger.warning("No parameter named %s. Known parameters: %s",
                                param_name,
-                               ', '.join(str(key) for key in result.keys()))
+                               ', '.join(str(key) for key in result.keys())
+                )
 
         return detailed_description.split(":param")[0], result
 
