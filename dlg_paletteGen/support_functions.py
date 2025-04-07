d6ce2c73583ff456fcd7fd51b92b98cfc0a862a2
Ryan Bunney <ryanbunney@fastmail.com>
2025-04-07 15:32:43 +0800

    Fix linter issues

--- dlg_paletteGen/support_functions.py
+++ dlg_paletteGen/support_functions.py
@@ -644,7 +644,7 @@ def import_using_name(mod_name: str, traverse: bool = False, err_log=True):
                             ) from e
             else:
                 logger.debug("Recursive import failed! %s", parts[0] in sys.modules)
-                return None
+                raise ModuleNotFoundError
     logger.debug("Loaded module: %s version: %s", mod_name, mod_version)
     return mod
 
